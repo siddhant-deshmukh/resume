6a26feca0d49e9c32660fd3c5182a4b6eeb5200c
siddhant-deshmukh <siddhant946044@gmail.com>
2023-04-21 17:10:02 +0530

    checkpoint for what going to do improved twiiter clone section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   darkMode:'class',
   content: ["./**/*.{html,js}"],
   theme: {
+    screens: {
+      xsm:'450px',
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {},
   },
   plugins: [],
