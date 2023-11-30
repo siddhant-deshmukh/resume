ed8d18420cfd87e922ef314be4c359c1f7155eee
siddhant-deshmukh <siddhant946044@gmail.com>
2023-11-30 23:50:10 +0530

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,22 @@ module.exports = {
       lg: '976px',
       xl: '1440px',
     },
-    extend: {},
+    extend: {
+      colors: {
+        // base: '#0b0c10',
+        main: '#171c28',
+        primary: 'white',
+        'primary-light-1' : 'rgb(249 250 251)',
+        'primary-light-2' : 'rgb(243 244 246)',
+        'primary-light-3' : 'rgb(229 231 235)',
+        // secondary: '#66fcf2'
+        // secondary: '#007bff',
+        'secondary': '#00f8f8',
+        'secondary-1': '#00baba',
+        'secondary-2': '#007c7c',
+        'secondary-3': '#003e3e'
+      }
+    },
   },
   plugins: [],
 }
